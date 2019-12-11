1fd232286b2ddb8e15423ea746cdcd2333ac6fbf
Markreel <markmeerssman@live.nl>
2019-12-11 17:28:26 +0100

    Added webcam functionality. Fixed git ignore.

---
+++ HybridSpace/Assets/Scripts/WebcamHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebcamHandler : MonoBehaviour
+{
+    [SerializeField] Material webcamMaterial;
+
+    private void Start()
+    {
+        StartWebcamVideo();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void StartWebcamVideo()
+    {
+        WebCamTexture _webcamTexture = new WebCamTexture();
+
+        _webcamTexture.deviceName = WebCamTexture.devices[1].name;
+        //OutputImage.gameObject.SetActive(true);
+        //OutputImage.texture = _webcamTexture;
+        //OutputImage.material.mainTexture = _webcamTexture;
+
+        webcamMaterial.mainTexture = _webcamTexture;
+
+        _webcamTexture.Play();
+    }
+}
